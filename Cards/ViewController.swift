7cf65ee0db128f9fab57495683e3ac9c00fa8e22
bristol <oro.eto.bristol@gmail.com>
2023-04-29 12:06:12 +0300

    Initial Commit

---
+++ Cards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cards
+//
+//  Created by Apple M1 on 29.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
